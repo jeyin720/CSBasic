0f3ad193911a979a228cd7eb931e875f3d8b5ed5
Park Yejin <s2018s30@e-mirim.hs.kr>
2020-04-28 14:22:24 +0900

    출력

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,10 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            var name = "윤" + "인" + "성";
+            Console.WriteLine(name);
+            Console.WriteLine("한빛 \t 아카데미");
+            Console.WriteLine("\"\"");
         }
     }
 }
